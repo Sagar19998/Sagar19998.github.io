5a76c2199c93dee7f08327bbc8c7ae5da17deb9b
Sagar jadhao <jadhaosagar9798@gmail.com>
2023-03-11 07:44:56 -0800

    portfolio done

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function resumeDownload(){
+   
+    window.open("Sagar-Jadhao-Resume (6).pdf","_blank");
+  }
+
+
+
+
+
+function myPort(){
+    alert("You are already live on My Portfolio!")
+}
+
+function myEd(){
+    location.href = 'index.html';
+}
+
+
+  
