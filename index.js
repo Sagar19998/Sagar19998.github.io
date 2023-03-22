40cb5cee543eb1a24a8948c9851f1b4a535baca2
Sagar jadhao <jadhaosagar9798@gmail.com>
2023-03-22 18:04:57 +0530

    navbar changed

--- index.js
+++ index.js
@@ -13,3 +13,5 @@ var runredirect = function () {
     var redirectw1 = window.open('https://drive.google.com/file/d/1YRee2Y_z7XpR4Goj_WYS2QlVLD_LDKQM/view?usp=sharing');
     redirectw1.location;
 }
+
+
